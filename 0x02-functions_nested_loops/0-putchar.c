b4104a7614156416126f781462391d3bd368e4df
solomon <solomonfkd@gmail.com>
2022-10-18 04:43:52 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "_putchar.h"
+int main(void)
+{
+	char *sh = "_putchar";
+		while (*sh)
+		{
+			_putchar(*sh);
+			sh++;
+		}
+		_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef main
+#define main
+
+//char _putchar(char c);
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+
+#endif /*Main_H */
