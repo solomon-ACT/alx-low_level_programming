3e54cb160b91ff7908aaac1ae27c57dc15b57651
solomon <solomonfkd@gmail.com>
2022-10-19 12:00:38 -0700

    modify

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,6 +3,7 @@
  * add - a function that adds two integers and returns the result
  * @one: The First Number
  * @two: The Scond Number
+ * Return: The Sum Of one + two
  */
 int add(int one, int two)
 {
