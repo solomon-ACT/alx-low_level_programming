9e3de9588c6bee074c80f63bb88b900dcec99991
solomon <solomonfkd@gmail.com>
2022-10-21 01:48:46 -0700

    modify

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,9 +6,9 @@
  */
 void print_number(int n)
 {
-long m; /* power of 10 */
+int m; /* power of 10 */
 int c; /* boolean check */
-long num; /* convert int to long */
+int num; /* convert int to long */
 	num = n;
 /* negatives */
 	if (num < 0)
