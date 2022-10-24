a4c1f33f53be02e0b36b3864b0e9e66979169f0a
solomon <solomonfkd@gmail.com>
2022-10-24 04:49:50 -0700

    add 100,101

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ *  * _atoi - Entry point
+ *   * @s: input
+ *    * Return: Always 0 (Success)
+ */
+int _atoi(char *s)
+{
+int x = atoi(s);
+return (x);
+}
+
