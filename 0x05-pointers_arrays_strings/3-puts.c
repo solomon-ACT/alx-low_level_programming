c30456e30e5ee51633342e9cd306896a745b98bb
solomon <solomonfkd@gmail.com>
2022-10-24 03:24:25 -0700

    Adding pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * puts2 - function should print only one character out of two
+ *   * starting with the first one
+ *    * @str: input
+ *     * Return: print
+ */
+void _puts(char *str)
+{
+int i;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
